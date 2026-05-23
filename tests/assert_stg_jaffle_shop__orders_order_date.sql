423869a0aa5ab880b2a96abfd48162f52c7236c7
tomszy91 <t.szyszlo+GIT@gmail.com>
2026-05-23 19:13:15 +0200

    marts_tests: tests added to marts tables. Singular tests refactored as macro tests

--- tests/assert_stg_jaffle_shop__orders_order_date.sql
+++
@@ -1,7 +0,0 @@
-select
-    order_id,
-    order_date
-from
-    {{ ref("stg_jaffle_shop__orders") }}
-where
-    order_date < '1900-01-01'
--- tests/assert_stg_stripe__payments_negative_amount.sql
+++
@@ -1,7 +0,0 @@
-select
-    payment_id,
-    amount
-from
-    {{ ref("stg_stripe__payments") }}
-where
-    amount < 0
